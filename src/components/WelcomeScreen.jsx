442361a688dba8b769348411f4dc4d44c1362d77
Misael Guerrero <mguerrerowest@gmail.com>
2024-05-27 15:17:13 -0600

    Give elements class names

--- src/components/WelcomeScreen.jsx
+++ src/components/WelcomeScreen.jsx
@@ -3,15 +3,19 @@ import { Link } from "react-router-dom"
 export default function WelcomeScreen(){
     return(
         <main>
-            <h1>
-                Fakestagram
-            </h1>
-            <div>
-                userIcon
-            </div>
-            <Link to={'/login'}>Login</Link>
-            <div>
-                Don't have an account yet? <Link to={'/register'}>Register Here</Link>
+            <div className="welcome">
+                <h1 className="welcome-logo">
+                    Fakestagram
+                </h1>
+                <div className="welcome-icon">
+                    userIcon
+                </div>
+                <div className="welcome-links">
+                    <Link to={'/login'}>Login</Link>
+                    <div>
+                        Don't have an account yet? <Link to={'/register'}>Register Here</Link>
+                    </div>
+                </div>
             </div>
         </main>
     )
