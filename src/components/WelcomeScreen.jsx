0307d3b62d489f7eeea0ddb7f1fb2e6704a0976a
Misael Guerrero <mguerrerowest@gmail.com>
2024-05-27 14:13:49 -0600

    Create WelcomeScreen component inside components directory

---
+++ src/components/WelcomeScreen.jsx
@@ -0,0 +1,16 @@
+export default function WelcomeScreen(){
+    return(
+        <main>
+            <h1>
+                Fakestagram
+            </h1>
+            <div>
+                userIcon
+            </div>
+            <a href="">Login</a>
+            <div>
+                Don't have an account yet? <a href="">Register Here</a>
+            </div>
+        </main>
+    )
+}
