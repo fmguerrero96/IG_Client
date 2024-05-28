f8e2d29bbf9fccd396bbed6281e68bf6b2990282
Misael Guerrero <mguerrerowest@gmail.com>
2024-05-28 15:25:14 -0600

    Create gallery component

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,10 @@
+export default function Gallery(){
+    return(
+        <div className="gallery">
+            <img className="img1" src="../src/assets/img1.jpg" alt="" />
+            <img className="img2" src="../src/assets/img2.jpg" alt="" />
+            <img className="img3" src="../src/assets/img3.jpg" alt="" />
+            <img className="img4" src="../src/assets/img4.jpg" alt="" />
+        </div>
+    )
+}
