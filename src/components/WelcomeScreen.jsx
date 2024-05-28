8a447e9fb8385d922634b20875edeacd5234d5b9
Misael Guerrero <mguerrerowest@gmail.com>
2024-05-28 15:26:06 -0600

    Include Gallery component and account circle png

--- src/components/WelcomeScreen.jsx
+++ src/components/WelcomeScreen.jsx
@@ -1,19 +1,23 @@
 import { Link } from "react-router-dom"
+import Gallery from "./Gallery"
 
 export default function WelcomeScreen(){
     return(
         <main>
-            <div className="welcome">
-                <h1 className="welcome-logo">
-                    Fakestagram
-                </h1>
-                <div className="welcome-icon">
-                    userIcon
-                </div>
-                <div className="welcome-links">
-                    <Link to={'/login'}>Login</Link>
-                    <div>
-                        Don't have an account yet? <Link to={'/register'}>Register Here</Link>
+            <div className="welcome-screen">
+                <Gallery/>
+                <div className="welcome">
+                    <h1 className="welcome-logo">
+                        Fakestagram
+                    </h1>
+                    <div className="welcome-icon">
+                        <img src="../src/assets/account_circle.png" alt="" />
+                    </div>
+                    <div className="welcome-links">
+                        <Link className="login-link" to={'/login'}>Login</Link>
+                        <div>
+                            Don't have an account yet? <Link to={'/register'}>Register Here</Link>
+                        </div>
                     </div>
                 </div>
             </div>
