93196341d58489cc284fa6267aa39554a6ab080b
Misael Guerrero <mguerrerowest@gmail.com>
2024-05-29 19:48:46 -0600

    Create a route for LoginPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import WelcomeScreen from './components/WelcomeScreen'
+import LoginPage from './components/LoginPage'
 import {Route, Routes} from "react-router-dom"
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
   return (
     <Routes>
       <Route path='/' element={<WelcomeScreen/>} />
+      <Route path='/login' element={<LoginPage/>} />
     </Routes>
   )
 }
