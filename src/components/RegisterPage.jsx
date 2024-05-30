f12a4080a04a7c34f3fc8ba99d1bae3c8b0996f1
Misael Guerrero <mguerrerowest@gmail.com>
2024-05-30 17:27:08 -0600

    Add a value and an onChange funciton to inputs

--- src/components/RegisterPage.jsx
+++ src/components/RegisterPage.jsx
@@ -3,13 +3,19 @@ import { useState } from "react"
 export default function RegisterPage() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    
+
 
     return(
         <form className="register">
             <h1>Register</h1>
-            <input type="text" placeholder="Username" required />
-            <input type="password" placeholder="Password" required />
+            <input type="text" 
+                placeholder="Username" required
+                value={username}
+                onChange={e => setUsername(e.target.value)} />
+            <input type="password" 
+                placeholder="Password" required
+                value={password}
+                onChange={e => setPassword(e.target.value, console.log(password))} />
             <button>Register</button>
         </form>
     )
