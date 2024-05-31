28d5404fdd02fbdd5adfcdef049945e6df864ae4
Misael Guerrero <mguerrerowest@gmail.com>
2024-05-31 16:29:30 -0600

    Create fetch request and provide error/success messages for user

--- src/components/RegisterPage.jsx
+++ src/components/RegisterPage.jsx
@@ -3,19 +3,49 @@ import { useState } from "react"
 export default function RegisterPage() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
 
+    const handleRegister = async (e) => {
+        e.preventDefault();
+        setError('');
+        setSuccess('');
+
+        try {
+            const response = await fetch('http://localhost:3000/users', {
+                method: 'POST',
+                body: JSON.stringify({username, password}),
+                headers: {'Content-Type': 'application/json'}
+            })
+
+            if(!response.ok){
+                const errorData = await response.json()
+                setError(errorData.error[0].msg || errorData.error);
+                throw new Error(errorData.error[0].msg || errorData.error || 'Registration failed');
+            }
+            setSuccess('Registration successful! You can now log in.');
+            setPassword('')
+            setUsername('')
+        }  catch(err) {
+            console.log(err)
+        } 
+    }
 
     return(
-        <form className="register">
+        <form className="register" onSubmit={handleRegister}>
             <h1>Register</h1>
+            {error && <div className="error">{error}</div>}
+            {success && <div className="success">{success}</div>}
             <input type="text" 
                 placeholder="Username" required
                 value={username}
-                onChange={e => setUsername(e.target.value)} />
+                onChange={e => setUsername(e.target.value)}
+                 />
             <input type="password" 
                 placeholder="Password" required
                 value={password}
-                onChange={e => setPassword(e.target.value, console.log(password))} />
+                onChange={e => setPassword(e.target.value)}
+                 />
             <button>Register</button>
         </form>
     )
