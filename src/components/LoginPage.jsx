e3193b56dc78f2f8a922fd70ccd4b532e78f4b36
Misael Guerrero <mguerrerowest@gmail.com>
2024-06-02 14:07:28 -0600

    Create handleLogin funciton and display error if there are any

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -3,9 +3,30 @@ import { useState } from "react";
 export default function LoginPage() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState('')
+
+    const handleLogin = async (e) => {
+         e.preventDefault();
+
+         try{
+            const response = await fetch('http://localhost:3000/users/login', {
+                method: 'POST',
+                body: JSON.stringify({username, password}),
+                headers: {'Content-Type': 'application/json'}
+            })
+
+            if(response !== 200){
+                const errorData = await response.json()
+                console.log(errorData.error)
+                setError(errorData)
+            }
+         } catch(err) {
+
+         }
+    }
 
     return(
-        <form className="login">
+        <form className="login" onSubmit={handleLogin}>
             <h1>Login</h1>
             <input type="text" placeholder="Username" 
             required
@@ -18,6 +39,7 @@ export default function LoginPage() {
             onChange={e => setPassword(e.target.value)} 
             />
             <button>Login</button>
+            {error && <div>{error.error}</div>}
         </form>
     )
 }
