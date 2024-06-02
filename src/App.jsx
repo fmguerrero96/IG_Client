facbd79b008ead88ab5154e70404b4c06bdd4b4e
Misael Guerrero <mguerrerowest@gmail.com>
2024-06-02 15:37:14 -0600

    Create route for HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import WelcomeScreen from './components/WelcomeScreen'
 import LoginPage from './components/LoginPage'
 import RegisterPage from './components/RegisterPage'
+import HomePage from './components/HomePage'
 import {Route, Routes} from "react-router-dom"
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Route path='/' element={<WelcomeScreen/>} />
       <Route path='/login' element={<LoginPage/>} />
       <Route path='/register' element={<RegisterPage/>} />
+      <Route path='/homePage' element={<HomePage/>} />
     </Routes>
   )
 }
