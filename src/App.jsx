6f9a03615208b7e167258095fe7935eb700b9594
Misael Guerrero <mguerrerowest@gmail.com>
2024-06-04 16:19:49 -0600

    Wrap all routes inside UserContextProvider

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,21 @@ import LoginPage from './components/LoginPage'
 import RegisterPage from './components/RegisterPage'
 import HomePage from './components/HomePage'
 import {Route, Routes} from "react-router-dom"
+import { UserContextProvider } from './UserContext'
 
 function App() {
   
 
   return (
-    <Routes>
-      <Route path='/' element={<WelcomeScreen/>} />
-      <Route path='/login' element={<LoginPage/>} />
-      <Route path='/register' element={<RegisterPage/>} />
-      <Route path='/homePage' element={<HomePage/>} />
-    </Routes>
+    <UserContextProvider>
+      <Routes>
+        <Route path='/' element={<WelcomeScreen/>} />
+        <Route path='/login' element={<LoginPage/>} />
+        <Route path='/register' element={<RegisterPage/>} />
+        <Route path='/homePage' element={<HomePage/>} />
+      </Routes>
+    </UserContextProvider>
+    
   )
 }
 
