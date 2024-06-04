2498efec12fee344903ffd11c434b7fb0d6bec8b
Misael Guerrero <mguerrerowest@gmail.com>
2024-06-03 18:03:50 -0600

    Create test fetch request to test protected api routes

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,5 +1,21 @@
+import { useEffect } from "react"
+
 export default function HomePage() {
+
+    useEffect(() => {
+        async function test(){
+            const data = await fetch('http://localhost:3000/protected', {
+                credentials: 'include'
+            })
+            if (data) {
+                data.json()
+                console.log(data)
+            }
+        }
+        test() 
+    })
+        
     return(
-        <div>HomePage</div>
+        <div>Hello </div>
     )
 };
