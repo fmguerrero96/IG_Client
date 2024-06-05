5112c2a4dcf7abf4016ae501f0131e6ee5ef785d
Misael Guerrero <mguerrerowest@gmail.com>
2024-06-05 15:32:19 -0600

    Create footer and layout components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+
+export default function Footer(){
+    return(
+        <footer>
+            <Link>Feed</Link>
+            <Link>New Post</Link>
+            <Link>Profile</Link>
+        </footer>
+    )
+};
