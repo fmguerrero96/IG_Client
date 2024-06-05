40d660da4fca0531dd214e5f502cd553411c39c7
Misael Guerrero <mguerrerowest@gmail.com>
2024-06-05 15:56:09 -0600

    Display username from UserContext state

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,17 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../UserContext";
 
 export default function Header() {
+    const {userInfo} = useContext(UserContext);
     return(
         <header>
             <Link to={'/homePage'} className="header-logo">Fakestagram</Link>
 
             <nav>
-                <p>Hello: 'Username'</p>
+                <p>Welcome: {userInfo.username}</p>
                 <p>Logout</p>
             </nav>
         </header>
     )
-}
+};
