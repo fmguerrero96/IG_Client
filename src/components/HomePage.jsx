94295d56602a6f7d9a467da2fbbe263d9f84d0d7
Misael Guerrero <mguerrerowest@gmail.com>
2024-06-06 12:13:37 -0600

    HomePage not implemented yet

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,21 +1,10 @@
 import { useEffect } from "react"
 
 export default function HomePage() {
-
-    useEffect(() => {
-        async function test(){
-            const data = await fetch('http://localhost:3000/protected', {
-                credentials: 'include'
-            })
-            if (data) {
-                data.json()
-                console.log(data)
-            }
-        }
-        test() 
-    })
         
     return(
-        <div>Hello </div>
+        <main className="home-page">
+            <div>feed (not implemented)</div>
+        </main>
     )
 };
