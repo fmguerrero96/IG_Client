9f79aa5d3787fb2653ef1d8a2c6f21df1e9b777a
Misael Guerrero <mguerrerowest@gmail.com>
2024-06-06 14:49:03 -0600

    Create new post form

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,7 +1,22 @@
 export default function CreatePost() {
     return(
         <div className="create-post">
-            Create new post (not implemented)
+            <form >
+                <h2>Create new post</h2>
+
+                <input type="file"
+                name="picture"
+                required
+                accept=".png, .jpg, .jpeg " />
+
+                <textarea type="text" 
+                name="caption" 
+                maxLength={150}
+                required
+                placeholder="Your caption here(optional)" />
+
+                <button>Create Post</button>
+            </form>
         </div>
     )
 }
