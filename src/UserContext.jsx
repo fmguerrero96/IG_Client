3f523a7afbf5cafba04f1fdb293b9b97b427f869
Misael Guerrero <mguerrerowest@gmail.com>
2024-06-05 21:46:57 -0600

    Create logout function

--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -27,8 +27,23 @@ export function UserContextProvider({children}){
         getUser()
     }, []);
 
+    async function logout() {
+        try{
+            const response = await fetch('http://localhost:3000/logout', {
+                method: 'POST',
+                credentials: 'include',
+            })
+            if(response.ok){
+                setUserInfo(null)
+            }
+        } catch(err) {
+            console.error('Error logging out:', err);
+        }
+        
+    }
+
     return(
-        <UserContext.Provider value={{userInfo, setUserInfo}}>
+        <UserContext.Provider value={{userInfo, setUserInfo, logout}}>
             {children}
         </UserContext.Provider>
     )
