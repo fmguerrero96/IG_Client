d91a48162e6b644d55dc40ee063c563badef1828
Misael Guerrero <mguerrerowest@gmail.com>
2024-06-11 12:46:39 -0600

    Make fetch request to get user's profile info

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,7 +2,26 @@ import { useState, useEffect, useContext } from "react";
 import { UserContext } from "../UserContext";
 
 export default function Profile() {
+    const [userProfile, setUserProfile] = useState({})
+    const { userInfo } = useContext(UserContext);
 
+    useEffect(() => {
+        const getProfile = async () => {
+            try{
+                const response = await fetch('http://localhost:3000/user', {
+                    credentials: 'include',
+                })
+
+                if(response.ok){
+                    setUserProfile(await response.json())
+                }
+            } catch(err) {
+                console.log(err)
+            }
+        }
+
+        getProfile()
+    }, []);
 
     return(
         <div className="profile">
