b41408ff16400ffdf47943d7349e66cc3da36476
Misael Guerrero <mguerrerowest@gmail.com>
2024-06-11 11:09:21 -0600

    Import hooks and UserContext

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,4 +1,9 @@
+import { useState, useEffect, useContext } from "react";
+import { UserContext } from "../UserContext";
+
 export default function Profile() {
+
+
     return(
         <div className="profile">
             Not implemented
