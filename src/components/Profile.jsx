bbed3dcdd8bab4614b4bd2d2b0552d3448fdda3d
Misael Guerrero <mguerrerowest@gmail.com>
2024-06-12 22:37:28 -0600

    ProfileInfo component was unnecessary so move logic back into Profile component and delete ProfileInfo

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,59 @@
-import ProfileInfo from "./ProfileInfo";
+import { useState, useEffect } from "react";
 
 export default function Profile() {
+    const [userProfile, setUserProfile] = useState({})
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        const getProfile = async () => {
+            try{
+                const response = await fetch('http://localhost:3000/user', {
+                    credentials: 'include',
+                })
+
+                if(response.ok){
+                    const profile = await response.json()
+                    setUserProfile(profile)
+                    setPosts(profile.posts)
+                }
+            } catch(err) {
+                console.log(err)
+            }
+        }
+
+        getProfile()
+    }, []);
 
     return(
-        <ProfileInfo/>
+        <main className="profile">
+            <div className="profile-info">
+                <span>
+                    <img className="profile-pic" src="../src/assets/account_circle.png" alt="profile picture" />
+                    <span>{userProfile.username}</span>
+                </span>
+                <span className="post-count">
+                    <span>{userProfile.posts ? (userProfile.posts.length) : (0)}</span>
+                    <span>Posts</span>
+                </span>
+                <span className="follower-count">
+                    <span>{userProfile.followers ? (userProfile.followers.length) : (0)}</span>
+                    <span>Followers</span>
+                </span>
+                <span className="following-count">
+                    <span>{userProfile.following ? (userProfile.following.length) : (0)}</span>
+                    <span>Following</span>
+                </span>
+            </div>
+
+            <div className="prfile-gallery">
+                {posts.length >0 ? (
+                     posts.map(post => (
+                        <img src={`http://localhost:3000/${post.picture}`} key={post._id}/>
+                     ))
+                ) : (
+                   <div>No posts yet</div>
+                )}
+            </div>
+        </main>
     )
 };
