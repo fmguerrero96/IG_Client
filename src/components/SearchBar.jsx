a536b80865857815fa2aa5deb901db3371016761
Misael Guerrero <mguerrerowest@gmail.com>
2024-06-18 12:48:51 -0600

    Set a use effect to set redirect variable back to false and set username variable back to empty string

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Navigate } from "react-router-dom";
 
 export default function SearchBar () {
     const [username, setUsername] = useState('');
     const [user, setUser] = useState(null);
     const [redirect, setRedirect] = useState(false);
+
+    useEffect(() => {
+        if(redirect === true){
+            setRedirect(false)
+        }
+    },[redirect]);
 
     const handleSearch = async (e) => {
         e.preventDefault()
@@ -18,6 +24,7 @@ export default function SearchBar () {
                 const user = await response.json();
                 setUser(user)
                 setRedirect(true) // Redirect to the user's profile
+                setUsername('')
             } else {
                 alert('User not found');
             }
