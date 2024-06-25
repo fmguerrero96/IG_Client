f12a113328532ce3e6ddde4831b8cae6df53cabc
Misael Guerrero <mguerrerowest@gmail.com>
2024-06-25 13:50:56 -0600

    Create fetch request to get the posts for the feed

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,35 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react";
 
 export default function HomePage() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchFeed = async () => {
+            try {
+                const response = await fetch(`http://localhost:3000/feed/`, {
+                    credentials: 'include',
+                    method: 'GET'
+                });
+
+                if (response.ok) {
+                    const feed = await response.json();
+                    setPosts(feed);
+                } else {
+                    console.error('Failed to fetch feed');
+                }
+            } catch (error) {
+                console.error('Error fetching feed:', error);
+            }
+        };
+
+        fetchFeed();
+    }, []);
         
     return(
         <main className="home-page">
-            <div>feed (not implemented)</div>
+            <div>
+                not implemented
+            </div>
         </main>
     )
 };
