e9d20008acc5508b289d1dc9e7dd2d7638aa9d04
Misael Guerrero <mguerrerowest@gmail.com>
2024-06-27 18:28:53 -0600

    Display all necessary info under the image

--- src/components/FeedCard.jsx
+++ src/components/FeedCard.jsx
@@ -1,16 +1,23 @@
+import { format } from "date-fns";
+
 export default function FeedCard({ posts }){
     return(
-        <div>
+        <div className="feed">
             {posts.length > 0 ? (
-            posts.map(post => (
-                <div key={post._id} className="post-card">
-                    <img src={`http://localhost:3000/${post.picture}`} />
-                    <div>{post.caption}</div>
-                    <div>Posted by: {post.author.username}</div>
-                    <div>{new Date(post.time_stamp).toLocaleString()}</div>
-                </div>
-            ))
-            ) : (
+                posts.map(post => (
+                    <div key={post._id} className="post-card">
+                        <img src={`http://localhost:3000/${post.picture}`} />
+
+                        <div className="post-info">
+                            <span>By: {post.author.username}</span>
+                            <span>{post.caption}</span>
+                            <span>Likes: {post.likes_count}</span>
+                            <span>Comments: {post.comments.length}</span>
+                            <span>{format(post.time_stamp, 'MMM d, yyyy HH:mm') }</span>
+                        </div>
+                    </div>
+                ))
+                ) : (
                 <div>No Posts to display</div>
             )}
         </div>
