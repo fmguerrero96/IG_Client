77df216798ea46d9606f394a72ea6452601820d2
Misael Guerrero <mguerrerowest@gmail.com>
2024-06-27 20:20:05 -0600

    Rearrange elements inside 'post-card' div

--- src/components/FeedCard.jsx
+++ src/components/FeedCard.jsx
@@ -9,12 +9,16 @@ export default function FeedCard({ posts }){
                         <img src={`http://localhost:3000/${post.picture}`} />
 
                         <div className="post-info">
-                            <span>By: {post.author.username}</span>
-                            <span>{post.caption}</span>
-                            <span>Likes: {post.likes_count}</span>
-                            <span>Comments: {post.comments.length}</span>
+                            <span className="author">{post.author.username}: 
+                                <span className="caption"> {post.caption}</span>
+                            </span>
+
+                            <span>{post.likes_count} likes</span>
+
+                            <span>{post.comments.length} comments</span>
                             <span>{format(post.time_stamp, 'MMM d, yyyy HH:mm') }</span>
                         </div>
+                        <hr />
                     </div>
                 ))
                 ) : (
