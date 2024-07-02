e19e5f8447624fa91c3aac51e6ec9901dcd94f47
Misael Guerrero <mguerrerowest@gmail.com>
2024-07-01 23:52:19 -0600

    Create function to handle like clicks

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -24,11 +24,33 @@ export default function HomePage() {
         };
 
         fetchFeed();
-    }, []);
+    }, [posts.likes_count]);
+
+    const handleLikeClick = async (postId, index) => {
+        try {
+            const response = await fetch(`http://localhost:3000/posts/${postId}/like`, {
+                method: 'POST',
+                credentials: 'include',
+            });
+
+            if (response.ok) {
+                const result = await response.json();
+                console.log(result)
+                const updatedPosts = [...posts];
+                updatedPosts[index].likes_count = result.likes_count;
+                updatedPosts[index].hasLiked = result.hasLiked;
+                setPosts(updatedPosts);
+            } else {
+                console.error('Failed to like/unlike post');
+            }
+        } catch (error) {
+            console.error('Error liking/unliking post:', error);
+        }
+    };
         
     return(
         <main className="home-page">
-            <FeedCard posts={posts}/>
+            <FeedCard posts={posts} handleLikeClick={handleLikeClick}/>
         </main>
     )
 };
