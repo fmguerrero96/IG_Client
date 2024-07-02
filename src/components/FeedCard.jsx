7c35632edefc115d07421e4f096204f603139895
Misael Guerrero <mguerrerowest@gmail.com>
2024-07-01 23:55:11 -0600

    Take in new handleLikeClick function as prop and modify heart icon's classname accordingly

--- src/components/FeedCard.jsx
+++ src/components/FeedCard.jsx
@@ -1,10 +1,10 @@
 import { format } from "date-fns";
 
-export default function FeedCard({ posts }){
+export default function FeedCard({ posts, handleLikeClick }){
     return(
         <div className="feed">
             {posts.length > 0 ? (
-                posts.map(post => (
+                posts.map((post, index) => (
                     <div key={post._id} className="post-card">
                         <img src={`http://localhost:3000/${post.picture}`} />
 
@@ -12,11 +12,13 @@ export default function FeedCard({ posts }){
                             <span className="author">{post.author.username}
                                 <span className="caption">...{post.caption}</span>
 
-                                <svg className="icon-heart" xmlns="http://www.w3.org/2000/svg" 
+                                <svg className={`icon-heart${post.hasLiked ? 'Liked' : ''}`} 
+                                    xmlns="http://www.w3.org/2000/svg" 
                                     height="24px" 
                                     viewBox="0 -960 960 960" 
                                     width="24px" 
-                                    fill="#e8eaed">
+                                    fill="#e8eaed"
+                                    onClick={() => handleLikeClick(post._id, index)}>
                                     <path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z"/>
                                 </svg>   
 
@@ -29,7 +31,7 @@ export default function FeedCard({ posts }){
                                 </svg>
                             </span>
 
-                            <span>{post.likes_count.length} likes</span>
+                            <span>{post.likes_count && post.likes_count.length} likes</span>
 
                             <span>{post.comments.length} comments</span>
                             <span>{format(post.time_stamp, 'MMM d, yyyy') }</span>
