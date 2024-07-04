aff73b1d0427db4cb6a72a79d5456bf5abdefbe8
Misael Guerrero <mguerrerowest@gmail.com>
2024-07-04 15:33:14 -0600

    Make fetch request to get all necessary post info and display post image

--- src/components/PostPage.jsx
+++ src/components/PostPage.jsx
@@ -1,5 +1,33 @@
+import { useContext, useState, useEffect } from "react";
+import { UserContext } from "../UserContext";
+import { useParams } from "react-router-dom";
+
 export default function PostPage() {
+    const {userInfo} = useContext(UserContext);
+    const { id } = useParams();
+    const [post, setPost] = useState({});
+
+    useEffect(() => {
+        const fetchPost = async () => {
+            try{
+                const response = await fetch(`http://localhost:3000/posts/${id}`, {
+                    credentials: 'include',
+                })
+                if (response.ok){
+                    const postInfo = await response.json();
+                    setPost(postInfo);
+                    console.log(postInfo)
+                }
+            } catch(err){
+                console.error('Error fetching user profile:', err);
+            }
+        }
+        fetchPost()
+    }, [id]);
+
     return(
-        <>Not Implemented Yet</>
+        <div  className="post-page">
+            <img className="post-picture" src={`http://localhost:3000/${post.picture}`}/>
+        </div>
     )
 };
