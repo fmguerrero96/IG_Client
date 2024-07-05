84a4b963eb99f2f9898aa3e393ddd99660eb2968
Misael Guerrero <mguerrerowest@gmail.com>
2024-07-04 23:03:45 -0600

    Display releveant post info

--- src/components/PostPage.jsx
+++ src/components/PostPage.jsx
@@ -1,6 +1,7 @@
 import { useContext, useState, useEffect } from "react";
 import { UserContext } from "../UserContext";
 import { useParams } from "react-router-dom";
+import { format } from "date-fns";
 
 export default function PostPage() {
     const {userInfo} = useContext(UserContext);
@@ -16,6 +17,7 @@ export default function PostPage() {
                 if (response.ok){
                     const postInfo = await response.json();
                     setPost(postInfo);
+                    console.log(postInfo)
                 }
             } catch(err){
                 console.error('Error fetching user profile:', err);
@@ -26,7 +28,31 @@ export default function PostPage() {
 
     return(
         <div  className="post-page">
-            <img className="post-picture" src={`http://localhost:3000/${post.picture}`}/>
+            <div className="single-post">
+                <img className="post-picture" src={`http://localhost:3000/${post.picture}`}/>
+                <div className="single-post-info">
+                    <div className="container">
+                        <span style={{fontWeight: '900'}}>{post.author?.username}...</span>
+                        <span>{post.caption}</span>
+
+                        <svg className={`icon-heart${post.hasLiked ? 'Liked' : ''}`} 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        height="24px" 
+                        viewBox="0 -960 960 960" 
+                        width="24px" 
+                        fill="#e8eaed"
+                        // onClick={() => handleLikeClick(post._id, index)}
+                        >
+                        <path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z"
+                        />
+                    </svg>
+                    </div>
+                    
+                    <p style={{marginBottom: '1%'}}>{post.likes_count?.length} Likes</p>
+                    <p>{post.time_stamp && format(post.time_stamp, 'MMM d, yyyy')}</p>
+                    
+                </div>
+            </div>
         </div>
     )
 };
