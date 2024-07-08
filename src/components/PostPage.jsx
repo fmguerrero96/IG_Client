f0c5e741bc81383d3923e4cf667e7eb2a199af2d
Misael Guerrero <mguerrerowest@gmail.com>
2024-07-08 13:20:28 -0600

    Handle like click on PostPage

--- src/components/PostPage.jsx
+++ src/components/PostPage.jsx
@@ -23,14 +23,34 @@ export default function PostPage() {
                         postInfo.hasLiked = false
                     }
                     setPost(postInfo);
-                    console.log(postInfo)
                 }
             } catch(err){
                 console.error('Error fetching user profile:', err);
             }
         }
         fetchPost()
-    }, [id]);
+    }, [post.likes_count]);
+
+    const handleLikeClick = async (postId) => {
+        try {
+            const response = await fetch(`http://localhost:3000/posts/${postId}/like`, {
+                method: 'POST',
+                credentials: 'include',
+            });
+
+            if (response.ok) {
+                const result = await response.json();
+                const updatedPost = post;
+                updatedPost.likes_count = result.likes_count;
+                updatedPost.hasLiked = result.hasLiked;
+                setPost(updatedPost);
+            } else {
+                console.error('Failed to like/unlike post');
+            }
+        } catch (error) {
+            console.error('Error liking/unliking post:', error);
+        }
+    };
 
     return(
         <div  className="post-page">
@@ -47,7 +67,7 @@ export default function PostPage() {
                         viewBox="0 -960 960 960" 
                         width="24px" 
                         fill="#e8eaed"
-                        // onClick={() => handleLikeClick(post._id, index)}
+                        onClick={() => handleLikeClick(post?._id)}
                         >
                         <path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z"
                         />
